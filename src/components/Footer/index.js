01933f68358d064f266e932128475db02ecf1f0e
Brandon Reid <brandonreid2@my.unt.edu>
2018-03-21 14:48:49 -0500

    took out nav from footer for now

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,18 +4,9 @@ import { Container } from 'reactstrap';
 export default (props) => (
   <footer>
     <Container>
-      <p>&copy; 2018. All Rights Reserved.</p>
-      <ul className="list-inline">
-        <li className="list-inline-item">
-          <a href="/">Contact</a>
-        </li>
-        <li className="list-inline-item">
-          <a href="/">Support</a>
-        </li>
-        <li className="list-inline-item">
-          <a href="/">FAQ</a>
-        </li>
-      </ul>
+      <div>
+        <p>&copy; 2018. All Rights Reserved.</p>
+      </div>
     </Container>
   </footer>
 );
