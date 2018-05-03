84f0862c58fb1cdf0631cecfa3faa23797802183
bar0191 <brandonreid2@my.unt.edu>
2018-05-03 13:12:29 -0500

    added google api map to report modal

--- src/components/ReportsModal/index.js
+++ src/components/ReportsModal/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import { Container, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import moment from 'moment';
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCLKqNMg-sZGJquZ32j1XTkHmrzj1tC0Is&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `300px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={16} defaultCenter={{ lat: props.lat[0], lng: props.long[0] }}>
+    {props.isMarkerShown && (
+      <Marker position={{ lat: props.lat[0], lng: props.long[0] }} />
+    )}
+  </GoogleMap>
+));
 
 export default (props) => (
   props.report ?
-    <Modal isOpen={props.isOpen} toggle={props.toggle} className={props.className} external={props.extBtn}>
+    <Modal style={{maxWidth: '750px'}} isOpen={props.isOpen} toggle={props.toggle} className={props.className} external={props.extBtn}>
       <ModalHeader>{props.report.title}</ModalHeader>
       <ModalBody>
         <div>
-          {props.report.imageOne ?
-            <img style={{maxWidth: '100%'}} src={"data:image/jpg;base64," + props.report.imageOne} /> :
-            <img style={{maxWidth: '100%'}} src={"data:image/jpg;base64," + props.report.mapSnapshot} />
-          }
+          <MyMapComponent
+            isMarkerShown
+            lat={Object.keys(props.report.coords ? props.report.coords : {}).map(function(key, index) {
+              console.log(props.report.coords['latitude']);
+              return props.report.coords['latitude'];
+            })}
+            long={Object.keys(props.report.coords ? props.report.coords : {}).map(function(key, index) {
+              return props.report.coords['longitude']
+            })}
+          />
+          {Object.keys(props.report.coords ? props.report.coords : {}).map(function(key, index) {
+            console.log(props.report.coords[key])
+          })}
           {/*<Carousel images={this.state.images} />*/}
         </div>
         <div style={{alignItems: 'center', marginHorizontal: 20, marginVertical: 15}}>
